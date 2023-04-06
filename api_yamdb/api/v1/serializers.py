01435e84bb52293caa131df343cad58fb2e12689
Владимир Стерлядкин <vssterlyadkin@gmail.com>
2023-04-06 19:17:26 +0700

    Ревью и комменты после ревью

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -1,7 +1,6 @@
 import re
 import uuid
 
-from django.shortcuts import get_object_or_404
 from django.db.models import Avg
 from rest_framework import serializers
 
@@ -159,10 +158,9 @@ class ReviewSerializer(serializers.ModelSerializer):
     def validate(self, data):
         request = self.context['request']
         title_id = self.context['view'].kwargs.get('title_id')
-        title = get_object_or_404(Title, pk=title_id)
         if request.method == 'POST':
             if Review.objects.filter(
-                title=title, author=request.user
+                title__id=title_id, author=request.user
             ).exists():
                 raise serializers.ValidationError(
                     "Нельзя добавить больше 1 комментария"
